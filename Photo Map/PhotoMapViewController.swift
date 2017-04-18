ceae89131ad2ff8bbf9bcda8194ba18a50c2de80
John Law <lawchihon@ymail.com>
2017-04-17 23:04:33 -0700

    Took a Photo

--- Photo Map/PhotoMapViewController.swift
+++ Photo Map/PhotoMapViewController.swift
@@ -13,6 +13,9 @@ class PhotoMapViewController: UIViewController {
 
     @IBOutlet var mapView: MKMapView!
     @IBOutlet weak var cameraButton: UIButton!
+    
+    var pickedImage: UIImage!
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -45,5 +48,32 @@ class PhotoMapViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+}
 
+extension PhotoMapViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    @IBAction func onCameraButton(_ sender: Any) {
+        let vc = UIImagePickerController()
+        vc.delegate = self
+        vc.allowsEditing = true
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            print("Camera is available 📸")
+            vc.sourceType = .camera
+        } else {
+            print("Camera 🚫 available so we will use photo library instead")
+            vc.sourceType = .photoLibrary
+        }
+        self.present(vc, animated: true, completion: nil)        
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController,
+                               didFinishPickingMediaWithInfo info: [String : Any]) {
+        // Get the image captured by the UIImagePickerController
+        self.pickedImage = info[UIImagePickerControllerEditedImage] as! UIImage
+        
+        // Do something with the images (based on your use case)
+        
+        // Dismiss UIImagePickerController to go back to your original view controller
+        self.dismiss(animated: true, completion: nil)
+        self.performSegue(withIdentifier: "tagSegue", sender: "")
+    }
 }
